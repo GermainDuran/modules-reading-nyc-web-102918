955c05d09b48376ef2a94d1a3ff833c12cfe3677
GermainDuran <arturodurantorres@gmail.com>
2018-11-06 13:03:19 -0500

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,18 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+include Dance  #To lend our two classes all of the methods of the Dance module, we use the include keyword
+extend MetaDancing #In order to lend a module's methods to a class as class methods, we use the extend keyword.
+extend FancyDance::ClassMethods
+ include FancyDance::InstanceMethods
+
+
+attr_accessor :name
+
+    def initialize(name)
+      @name = name
+    end
+
+end
